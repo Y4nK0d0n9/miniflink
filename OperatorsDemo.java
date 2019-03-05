fac07e8d67b783e39eef4a495b3a366a3357fb1f
dongyk <dongyk@zetyun.com>
2019-03-05 19:04:25 +0800

    add op: keyby

--- OperatorsDemo.java
+++ OperatorsDemo.java
@@ -19,7 +19,12 @@
 import org.apache.flink.configuration.ConfigConstants;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
+import sun.awt.SunHints;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class OperatorsDemo {
 
@@ -32,20 +37,60 @@ public class OperatorsDemo {
         conf.setBoolean(ConfigConstants.LOCAL_START_WEBSERVER, true);
         StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
 
+//        env.socketTextStream(hostname,9000,"\n")
+//                .map((value) -> {
+//                    return Math.pow(Integer.parseInt(value), 2);
+//                })
+//                .print();
+//
+//        env.socketTextStream(hostname,9001,"\n")
+//                .flatMap((String value, Collector<Integer> tmp) -> {
+//                    for (String intStr : value.split("\\s")) {
+//                        tmp.collect(Integer.parseInt(intStr) + 1);
+//                    }
+//                }).returns(Integer.class)
+//                .print();
+//
+//        env.socketTextStream(hostname,9000,"\n")
+//                .filter((value) -> {
+//                    return Integer.parseInt(value) < 10;
+//                }
+//                .print();
+
         env.socketTextStream(hostname,9000,"\n")
                 .map((value) -> {
-                    return Math.pow(Integer.parseInt(value), 2);
+                    Feature col = new Feature();
+                    col.setFeatureName("x1");
+                    col.setValues(Integer.parseInt(value));
+                    return col;
                 })
-                .print();
+                .keyBy("featureName")
+                .timeWindow(Time.seconds(1));
 
-        env.socketTextStream(hostname,9001,"\n")
-                .flatMap((String value, Collector<Integer> tmp) -> {
-                    for (String intStr : value.split("\\s")) {
-                        tmp.collect(Integer.parseInt(intStr) + 1);
-                    }
-                }).returns(Integer.class)
-                .print();
 
         env.execute("OperatorsDemo");
     }
+
+    public static class Feature {
+        public String getFeatureName() {
+            return featureName;
+        }
+
+        public void setFeatureName(String featureName) {
+            this.featureName = featureName;
+        }
+
+        String featureName;
+
+        public Integer getValues() {
+            return values;
+        }
+
+        public void setValues(Integer values) {
+            this.values = values;
+        }
+
+        Integer values;
+    }
 }
+
