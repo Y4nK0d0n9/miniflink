737d9a806fbc3eaa77485f077e1277619d792596
dongyk <dongyk@zetyun.com>
2019-03-06 18:02:08 +0800

    add keyselector

--- OperatorsDemo.java
+++ OperatorsDemo.java
@@ -16,18 +16,15 @@
  * limitations under the License.
  */
 
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.ConfigConstants;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
-import sun.awt.SunHints;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class OperatorsDemo {
-
     public static void main(String[] args) throws Exception {
 
         // the host and the port to connect to
@@ -42,7 +39,7 @@ public class OperatorsDemo {
 //                    return Math.pow(Integer.parseInt(value), 2);
 //                })
 //                .print();
-//
+
 //        env.socketTextStream(hostname,9001,"\n")
 //                .flatMap((String value, Collector<Integer> tmp) -> {
 //                    for (String intStr : value.split("\\s")) {
@@ -50,47 +47,76 @@ public class OperatorsDemo {
 //                    }
 //                }).returns(Integer.class)
 //                .print();
-//
+
 //        env.socketTextStream(hostname,9000,"\n")
 //                .filter((value) -> {
 //                    return Integer.parseInt(value) < 10;
 //                }
 //                .print();
 
+//        env.socketTextStream(hostname,9000,"\n")
+//                .map((value) -> {
+//                    Feature col = new Feature();
+//                    col.setFeatureName("x1");
+//                    col.setValues(Integer.parseInt(value));
+//                    return col;
+//                })
+//                .keyBy("featureName")
+//                .print();
+
+//        env.socketTextStream(hostname,9000,"\n")
+//                .map((value) -> {
+//                    Tuple2<String, String> features = new Tuple2();
+//                    Integer pos = 0;
+//                    for (String feature : value.split(",")) {
+//                        features.setField(feature, pos++);
+//                    }
+//                    return features;
+//                }).returns(new TypeHint<Tuple2<String, String>>(){})
+//                .keyBy(0)
+//                .print();
+
         env.socketTextStream(hostname,9000,"\n")
                 .map((value) -> {
-                    Feature col = new Feature();
-                    col.setFeatureName("x1");
-                    col.setValues(Integer.parseInt(value));
-                    return col;
+                    Tuple2<String, String> features = new Tuple2();
+                    Integer pos = 0;
+                    for (String feature : value.split(",")) {
+                        features.setField(feature, pos++);
+                    }
+                    return features;
+                }).returns(new TypeHint<Tuple2<String, String>>(){})
+                .keyBy((value) -> {
+                    return value.getField(0);
                 })
-                .keyBy("featureName")
-                .timeWindow(Time.seconds(1));
-
+                .print();
 
         env.execute("OperatorsDemo");
     }
 
-    public static class Feature {
-        public String getFeatureName() {
-            return featureName;
-        }
-
-        public void setFeatureName(String featureName) {
-            this.featureName = featureName;
-        }
-
-        String featureName;
-
-        public Integer getValues() {
-            return values;
-        }
-
-        public void setValues(Integer values) {
-            this.values = values;
-        }
-
-        Integer values;
-    }
+//    public static class Feature {
+//        public String getFeatureName() {
+//            return featureName;
+//        }
+//
+//        public void setFeatureName(String featureName) {
+//            this.featureName = featureName;
+//        }
+//
+//        String featureName;
+//
+//        public Integer getValues() {
+//            return values;
+//        }
+//
+//        public void setValues(Integer values) {
+//            this.values = values;
+//        }
+//
+//        Integer values;
+//
+//        public String toString() {
+//            return values.toString();
+//        }
+//    }
 }
 
